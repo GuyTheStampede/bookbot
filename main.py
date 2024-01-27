e686d0809279dba2f0d8ceccfba62504ca6a82af
guythestampede <guythestampede@gmail.com>
2024-01-27 12:59:18 -0600

    completed assignment to create dicionary of letters

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def main():
     final_results = {}
     book_path = "books/frankenstein.txt"
-
+        
     with open(book_path) as b:
         book_contents = b.read()
         #print (book_contents)
+    for b in book_contents.lower():
+        if b not in final_results:
+            final_results[b] = 1
+        elif b in final_results:
+            final_results[b] += 1
+        #print (b)
     
     print (final_results)
     
