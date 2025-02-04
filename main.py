f36bfb5d76142c208bd9926dc411089e752a4673
GuyTheStampede <guythestampede@gmail.com>
2025-02-03 22:53:58 -0600

    editing main.py to have a list and dic of character and word counts. currently prints out charcater count

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 with open("./books/frankenstein.txt") as f:
 	file_text = f.read()
-	print (file_text)
+	character_dict = {}
+	text_as_list = file_text.split()
+	for x in file_text:
+		x = x.lower()
+		if x not in character_dict:
+			character_dict[x] = 1
+		else:
+			character_dict[x] += 1
+		
+	print (character_dict)
